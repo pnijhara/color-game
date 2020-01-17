1777ec167f488f90561a1c1d080d3140c25a3d58
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-01-17 13:53:50 +0530

    Add js for picking color

---
+++ app.js
@@ -0,0 +1,31 @@
+alert("connected!");
+const squares = document.querySelectorAll('.square');
+const colors = [
+    'rgb(0, 0, 255)',
+    'rgb(0, 255, 255)',
+    'rgb(255, 0, 255)',
+    'rgb(0, 255, 0)',
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+];
+const pikedcolor = colors[4];
+
+// console.log(squares);
+
+for(var i = 0 ; i < squares.length; i++){
+    // console.log(squares[i]);
+    squares[i].style.backgroundColor = colors[i];
+    console.log(squares[i].style.backgroundColor)
+
+    squares[i].addEventListener('click', function(){
+        let clickedcolor = this.style.backgroundColor;
+
+        if(clickedcolor === pikedcolor){
+            alert('correct');
+        }
+        else{
+            alert("try again");
+        }
+
+    });
+}
